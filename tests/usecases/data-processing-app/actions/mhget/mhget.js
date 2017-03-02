86876827791e580e74009573461ce1966dcc98dc
David Liu <david.liu@cn.ibm.com>
2017-03-02 11:01:23 +0800

    Add message-hub use cases, for issue #139.

---
+++ tests/usecases/data-processing-app/actions/mhget/mhget.js
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2017 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ This function is bound to a trigger and is initiated when the message arrives
+ via OpenWhisk feed connected to Message Hub. Note that many messages can come in
+ as a large batch. Example input:
+{
+	"messages": [{
+		"partition": 0,
+		"key": null,
+		"offset": 252,
+		"topic": "in-topic",
+		"value": {
+			"events": [{
+				"eventType": "update",
+				"timestamp": "2011-01-01T11:11:11.111+02",
+				"payload": {
+					"velocity": 11,
+					"type": "taxi",
+					"name": "taxi1",
+					"location": {
+						"latitude": 11.11,
+						"longitude": -1.11
+					}
+				},
+				"deviceType": "taxi",
+				"orgId": "Org1",
+				"deviceId": "taxi1"
+			},
+			{ ... }
+			]
+		}
+	},
+	{ ... }
+	]
+}
+
+Expected output (merge all events from multiple 'messages' into one big 'events'):
+{
+	"events": [{
+		"eventType": "update",
+		"timestamp": "2011-01-01T11:11:11.111+02",
+		"payload": {
+			"velocity": 11,
+			"type": "taxi",
+			"name": "taxi1",
+			"location": {
+				"latitude": 11.11,
+				"longitude": -1.11
+			}
+		},
+		"deviceType": "taxi",
+		"orgId": "Org1",
+		"deviceId": "taxi1"
+	},
+	{ ... }
+	]
+
+}
+**/
+
+function main(params) {
+	console.log("DEBUG: received the following message as input: " + JSON.stringify(params));
+
+	return new Promise(function(resolve, reject) {
+		if (!params.messages || !params.messages[0] ||
+			!params.messages[0].value || !params.messages[0].value.events) {
+			reject("Invalid arguments. Must include 'messages' JSON array with 'value' field");
+		}
+		var msgs = params.messages;
+		var out = [];
+		for (var i = 0; i < msgs.length; i++) {
+			var msg = msgs[i];
+			for (var j = 0; j < msg.value.events.length; j++) {
+				out.push(msg.value.events[j]);
+			}
+		}
+		resolve({
+			"events": out
+		});
+	});
+}
---
+++ tests/usecases/data-processing-app/actions/mhpost/mhpost.js
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2017 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Analyze incoming message and generate a summary as a response
+ */
+function transform(events) {
+  var average = 0;
+  for (var i = 0; i < events.length; i++) {
+    average += events[i].payload.velocity;
+  }
+  average = average / events.length;
+  var result = {
+    "agent": "OpenWhisk action",
+    "events_count": events.length,
+    "avg_velocity": average
+  }
+  return result;
+}
+
+/**
+ * Process incoming message and publish it to Message Hub or Kafka.
+ * This code is used as the OpenWhisk Action implementation and linked to a trigger via a rule.
+ */
+function main(args) {
+  console.log("DEBUG: Received message as input: " + JSON.stringify(args));
+
+  return new Promise(function(resolve, reject) {
+    if (!args.topic || !args.events || !args.events[0] || !args.kafka_rest_url || !args.api_key)
+      reject("Error: Invalid arguments. Must include topic, events[], kafka_rest_url, api_key.");
+
+    // construct CF-style VCAP services JSON
+    var vcap_services = {
+      "messagehub": [{
+        "credentials": {
+          "kafka_rest_url": args.kafka_rest_url,
+          "api_key": args.api_key
+        }
+      }]
+    };
+
+    var MessageHub = require('message-hub-rest');
+    var kafka = new MessageHub(vcap_services);
+    var transformedMessage = transform(args.events)
+    console.log("DEBUG: Message to be published: " + JSON.stringify(transformedMessage));
+
+    kafka.produce(args.topic, transformedMessage)
+      .then(function() {
+        resolve({
+          "result": "Success: Message was sent to IBM Message Hub."
+        });
+      })
+      .fail(function(error) {
+        reject(error);
+      });
+  });
+}
