397c153b90907382faf954b28c81a7445b48176a
Vincent Hou <shou@us.ibm.com>
2017-03-16 16:02:11 -0400

    Add the missing windows binary

--- tools/travis/build_tag_releases.sh
+++ tools/travis/build_tag_releases.sh
@@ -2,16 +2,16 @@
 
 declare -a os_list=("linux" "darwin" "windows")
 arc=amd64
-file_name=${1:-"wskdeploy"}
+build_file_name=${1:-"wskdeploy"}
 
 for os in "${os_list[@]}"
 do
-    build_file_name=$file_name
+    wskdeploy=$build_file_name
     if [ "$os" == "windows" ]; then
-        build_file_name="$build_file_name.exe"
+        wskdeploy="$wskdeploy.exe"
     fi
     cd $TRAVIS_BUILD_DIR
-    GOOS=$os GOARCH=$arc go build -o build/$os/$build_file_name
+    GOOS=$os GOARCH=$arc go build -o build/$os/$wskdeploy
     cd build/$os
-    zip -r "$TRAVIS_BUILD_DIR/$build_file_name-$TRAVIS_TAG-$os-$arc.zip" $build_file_name
+    zip -r "$TRAVIS_BUILD_DIR/$build_file_name-$TRAVIS_TAG-$os-$arc.zip" $wskdeploy
 done
