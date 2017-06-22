1808890d6006f3307c2a718171161dd03896df77
Matt Rutkowski <mrutkows@us.ibm.com>
2017-06-22 14:19:06 -0500

    Enable scancode and update all files that did not pass.

---
+++ tools/travis/cloneutils.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -o xtrace
+
+SCRIPTDIR=$(cd $(dirname "$0") && pwd)
+HOMEDIR="$SCRIPTDIR/../../../"
+
+# clone OpenWhisk repo. in order to run scanCode.py
+cd $HOMEDIR
+git clone https://github.com/apache/incubator-openwhisk-utilities.git
