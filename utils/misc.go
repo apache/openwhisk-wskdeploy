ce936f78b71fd6bcc4fecee5cb14f0edc8c22d55
Matt Rutkowski <mrutkows@us.ibm.com>
2017-07-26 03:02:39 -0500

    Add logic that recognizes single vs multiline param schema.

--- utils/misc.go
+++ utils/misc.go
@@ -151,7 +151,7 @@ func GetEnvVar(key interface{}) interface{} {
 	}
 
 	if reflect.TypeOf(key).String() == "string" {
-		if isValidEnvironmentVar( key.(string)) {
+		if isValidEnvironmentVar(key.(string)) {
 			// retrieve the value of the env. var. from the host system.
 			envkey := strings.Split(key.(string), "$")[1]
 			value := os.Getenv(envkey)
@@ -159,11 +159,10 @@ func GetEnvVar(key interface{}) interface{} {
 				// TODO() We should issue a warning to the user (verbose) that env. var. was not found
 				// (i.e., and empty string was returned).
 			}
-			// TODO() We should issue a warning to the user (verbose) that env. var. was not found
-			// or had no value
-			return key.(string)
+			return value
 		}
-		// TODO() We should issue a warning to the user (verbose) that env. var. was not found
+
+		// The key was not a valid env. variable, simply return it as the value itself (of type string)
 		return key.(string)
 	}
 	return key
