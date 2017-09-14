a1d9f4e7ef24bd77b535592e6e11bc0f4af7e92c
daisy-ycguo <daisy.ycguo@gmail.com>
2017-09-14 10:14:05 +0000

    add sequence tests

---
+++ tests/src/integration/validateSequencesCreation/actions/function.js
@@ -0,0 +1,15 @@
+/**
+ * Return a simple string to 
+ * confirm this function has been visited.
+ *
+ * @param visited the visited function list
+ */
+function main(params) {
+    functionID = params.functionID || 'X'
+    if (params.visited == null) {
+        params.visited = 'function'+functionID;
+    } else {
+        params.visited = params.visited + ', function'+functionID;
+    }
+    return {"visited":params.visited};
+}
