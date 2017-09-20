a01b7aa7b550e0f656a3965fb952f269f15809a2
Matt Rutkowski <mrutkows@us.ibm.com>
2017-09-20 13:23:06 -0500

    Update and organize README topics; link to programming guide.

--- docs/examples/src/hello.js
+++ docs/examples/src/hello.js
@@ -1,3 +1,9 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more contributor
+// license agreements; and to You under the Apache License, Version 2.0.
+
+/*
+ * Hello, world
+ */
 function main(params) {
     msg = "Hello, " + params.name + " from " + params.place;
     return { payload:  msg };
