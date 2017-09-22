befabe152ba77e2a2d130de2ff114288620a5d8c
Vincent Hou <shou@us.ibm.com>
2017-09-22 10:00:40 -0400

    Add the 32-bit binaries build for each operating system

--- tools/travis/build_tag_releases.sh
+++ tools/travis/build_tag_releases.sh
@@ -1,17 +1,28 @@
 #!/usr/bin/env bash
 
 declare -a os_list=("linux" "darwin" "windows")
-arc=amd64
+declare -a arc_list=("amd64" "386")
 build_file_name=${1:-"wskdeploy"}
 
 for os in "${os_list[@]}"
 do
-    wskdeploy=$build_file_name
-    if [ "$os" == "windows" ]; then
-        wskdeploy="$wskdeploy.exe"
-    fi
-    cd $TRAVIS_BUILD_DIR
-    GOOS=$os GOARCH=$arc go build -o build/$os/$wskdeploy
-    cd build/$os
-    zip -r "$TRAVIS_BUILD_DIR/$build_file_name-$TRAVIS_TAG-$os-$arc.zip" $wskdeploy
+    for arc in "${arc_list[@]}"
+    do
+        wskdeploy=$build_file_name
+        os_name=$os
+        if [ "$os" == "windows" ]; then
+            wskdeploy="$wskdeploy.exe"
+        fi
+        if [ "$os" == "darwin" ]; then
+            os_name="mac"
+        fi
+        cd $TRAVIS_BUILD_DIR
+        GOOS=$os GOARCH=$arc go build -o build/$os/$wskdeploy
+        cd build/$os
+        if [[ "$os" == "linux" ]]; then
+            tar -czvf "$TRAVIS_BUILD_DIR/$build_file_name-$TRAVIS_TAG-$os_name-$arc.tgz" $wskdeploy
+        else
+            zip -r "$TRAVIS_BUILD_DIR/$build_file_name-$TRAVIS_TAG-$os_name-$arc.zip" $wskdeploy
+        fi
+    done
 done
