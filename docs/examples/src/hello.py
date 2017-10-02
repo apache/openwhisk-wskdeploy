2e3d4f8048a0007c11ed369ce560e14474f6feaa
Matt Rutkowski <mrutkows@us.ibm.com>
2017-10-02 15:10:52 -0500

    Add runtime version example.

---
+++ docs/examples/src/hello.py
@@ -0,0 +1,19 @@
+"""OpenWhisk "Hello world" in Python.
+
+// Licensed to the Apache Software Foundation (ASF) under one or more contributor
+// license agreements; and to You under the Apache License, Version 2.0.
+"""
+
+
+def main(dict):
+    """Hello world."""
+    if 'name' in dict:
+        name = dict['name']
+    else:
+        name = "stranger"
+    if 'place' in dict:
+        place = dict['place']
+    else:
+        place = "unknown"
+    msg = "Hello, " + name + " from " + place
+    return {"greeting": msg}
