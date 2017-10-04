91d925b05dbb26479f2629bc341667be969293f0
Matt Rutkowski <mrutkows@us.ibm.com>
2017-10-04 13:54:08 -0500

    Update sequence Actions to perform more tasks.

--- docs/examples/src/member_process.js
+++ docs/examples/src/member_process.js
@@ -1,19 +1,20 @@
 // Licensed to the Apache Software Foundation (ASF) under one or more contributor
 // license agreements; and to You under the Apache License, Version 2.0.
 
-// function formatDate(date) {
-//   var hours = date.getHours();
-//   var minutes = date.getMinutes();
-//   var ampm = hours >= 12 ? 'pm' : 'am';
-//   hours = hours % 12;
-//   hours = hours ? hours : 12; // the hour '0' should be '12'
-//   minutes = minutes < 10 ? '0'+minutes : minutes;
-//   var strTime = hours + ':' + minutes + ' ' + ampm;
-//   return date.getMonth()+1 + "/" + date.getDate() + "/" + date.getFullYear() + "  " + strTime;
-// }
+function formatDate(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return date.getMonth()+1 + "/" + date.getDate() + "/" + date.getFullYear() + "  " + strTime;
+}
 
 function main(params) {
   console.log("params: " + JSON.stringify(params, null, 4));
+
   let regionMap = new Map([
     ['Shire', 'Eriador'],
     ['the Shire', 'Eriador'],
@@ -32,6 +33,7 @@ function main(params) {
   if(params.member && typeof params.member === "object"){
     member = params.member;
     member.region = regionMap.get(member.place) || "unknown";
+    member.date = formatDate(new Date(member.joined))
   }
   else
     throw new Error("Invalid parameter: 'member'. type="+typeof(member)+" (expected object).")
