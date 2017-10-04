e5ab0a7f109a8d3b59b28c1a52365901c6dee6d8
Matt Rutkowski <mrutkows@us.ibm.com>
2017-10-04 14:04:57 -0500

    Update sequence Actions to perform more tasks.

--- docs/examples/src/member_process.js
+++ docs/examples/src/member_process.js
@@ -1,21 +1,6 @@
 // Licensed to the Apache Software Foundation (ASF) under one or more contributor
 // license agreements; and to You under the Apache License, Version 2.0.
-
-function formatDate(date) {
-  var hours = date.getHours();
-  var minutes = date.getMinutes();
-  var ampm = hours >= 12 ? 'pm' : 'am';
-  hours = hours % 12;
-  hours = hours ? hours : 12; // the hour '0' should be '12'
-  minutes = minutes < 10 ? '0'+minutes : minutes;
-  var strTime = hours + ':' + minutes + ' ' + ampm;
-  return date.getMonth()+1 + "/" + date.getDate() + "/" + date.getFullYear() + "  " + strTime;
-}
-
-function main(params) {
-  console.log("params: " + JSON.stringify(params, null, 4));
-
-  let regionMap = new Map([
+  const regionMap = new Map([
     ['Shire', 'Eriador'],
     ['the Shire', 'Eriador'],
     ['Hobbiton', 'Eriador'],
@@ -28,15 +13,19 @@ function main(params) {
     ['Minas Morgul', 'Mordor'],
   ]);
 
-  console.log("typeof=" + typeof params.member )
+
+function main(params) {
+  console.log("params: " + JSON.stringify(params, null, 4));
 
   if(params.member && typeof params.member === "object"){
+    // Augment the member (record) created in the previous Action
     member = params.member;
     member.region = regionMap.get(member.place) || "unknown";
-    member.date = formatDate(new Date(member.joined))
+    member.date = new Date(member.joined).toLocaleDateString();
+    member.time = new Date(member.joined).toLocaleTimeString();
   }
   else
-    throw new Error("Invalid parameter: 'member'. type="+typeof(member)+" (expected object).")
+    throw new Error("Invalid parameter: 'member'. type="+typeof(member) + ", expected object).")
 
   console.log("member: " + JSON.stringify(member, null, 4));
   return { member: member };
