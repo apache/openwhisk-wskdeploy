04ab99310c449654bb03cc2607fd1028b44ffd23
Matt Rutkowski <mrutkows@us.ibm.com>
2017-10-04 14:06:19 -0500

    Update sequence Actions to perform more tasks.

--- docs/examples/src/member_process.js
+++ docs/examples/src/member_process.js
@@ -1,6 +1,6 @@
 // Licensed to the Apache Software Foundation (ASF) under one or more contributor
 // license agreements; and to You under the Apache License, Version 2.0.
-  const regionMap = new Map([
+const regionMap = new Map([
     ['Shire', 'Eriador'],
     ['the Shire', 'Eriador'],
     ['Hobbiton', 'Eriador'],
@@ -11,22 +11,21 @@
     ['Dale', 'Rhovanion'],
     ['Lake Town', 'Rhovanion'],
     ['Minas Morgul', 'Mordor'],
-  ]);
-
+]);
 
 function main(params) {
-  console.log("params: " + JSON.stringify(params, null, 4));
+    console.log("params: " + JSON.stringify(params, null, 4));
 
-  if(params.member && typeof params.member === "object"){
-    // Augment the member (record) created in the previous Action
-    member = params.member;
-    member.region = regionMap.get(member.place) || "unknown";
-    member.date = new Date(member.joined).toLocaleDateString();
-    member.time = new Date(member.joined).toLocaleTimeString();
-  }
-  else
-    throw new Error("Invalid parameter: 'member'. type="+typeof(member) + ", expected object).")
+    if(params.member && typeof params.member === "object"){
+        // Augment the member (record) created in the previous Action
+        member = params.member;
+        member.region = regionMap.get(member.place) || "unknown";
+        member.date = new Date(member.joined).toLocaleDateString();
+        member.time = new Date(member.joined).toLocaleTimeString();
+    }
+    else
+        throw new Error("Invalid parameter: 'member'. type="+typeof(member) + ", expected object).")
 
-  console.log("member: " + JSON.stringify(member, null, 4));
-  return { member: member };
+    console.log("member: " + JSON.stringify(member, null, 4));
+    return { member: member };
 }
