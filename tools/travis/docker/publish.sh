e381a571c1c19a384d263454fbae542938ec5da3
Belinda Vennam <beemarie@us.ibm.com>
2017-10-05 15:13:36 -0500

    docker_user

--- tools/travis/docker/publish.sh
+++ tools/travis/docker/publish.sh
@@ -8,7 +8,7 @@ dockerhub_image_name="$2"
 dockerhub_image_tag="$3"
 dockerhub_image="${dockerhub_image_prefix}/${dockerhub_image_name}:${dockerhub_image_tag}"
 
-docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+docker login -u "${DOCKER_USER}" -p "${DOCKER_PASSWORD}"
 
 cp $TRAVIS_BUILD_DIR/build/linux/wskdeploy $SCRIPTDIR/
 
