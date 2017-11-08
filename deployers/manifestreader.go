dd10a9d62b24d248da0dd3d9b83c2112ababd9de
mptap <mptapasw@ncsu.edu>
2017-11-07 16:42:46 -0800

    Fixes #331 Delete dead code - manifestreader.go

--- deployers/manifestreader.go
+++ deployers/manifestreader.go
@@ -372,17 +372,3 @@ func (reader *ManifestReader) SetApis(ar []*whisk.ApiCreateRequest) error {
 	}
 	return nil
 }
-
-// from whisk go client
-func (deployer *ManifestReader) getQualifiedName(name string, namespace string) string {
-	if strings.HasPrefix(name, "/") {
-		return name
-	} else if strings.HasPrefix(namespace, "/") {
-		return fmt.Sprintf("%s/%s", namespace, name)
-	} else {
-		if len(namespace) == 0 {
-			namespace = clientConfig.Namespace
-		}
-		return fmt.Sprintf("/%s/%s", namespace, name)
-	}
-}
