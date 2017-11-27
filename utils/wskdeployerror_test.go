d89d847e466c14415834c24666cbc98732cf97cb
Matt Rutkowski <mrutkows@us.ibm.com>
2017-11-27 14:54:18 -0600

    Fix unit test that did not expect nested errors.

--- utils/wskdeployerror_test.go
+++ utils/wskdeployerror_test.go
@@ -169,7 +169,15 @@ func TestCustomErrorOutputFormat(t *testing.T) {
 	/*
 	 * YAMLParserErr
 	 */
-	// TODO
+
+	// TODO add a unti test once we re-factor error related modules into a new package
+	// to avoid cyclic deplendency errors in GoLang.
+	//manifestFile := "../tests/dat/manifest_bad_yaml_invalid_comment.yaml"
+	//// read and parse manifest.yaml file
+	//m, err := parsers.NewYAMLParser().ParseManifest(manifestFile)
+	//fmt.Println(err)
+
+	// TODO - use actual YAML files to generate actual errors for comparison with expected error output
 	//var TEST_LINES    = []string{"40", STR_UNKNOWN, "123"}
 	//var TEST_MESSAGES = []string{"did not find expected key", "did not find expected ',' or ']'", "found duplicate %YAML directive"}
 	//
