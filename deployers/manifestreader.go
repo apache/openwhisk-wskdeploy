170307bd8286ef59f9e0ecf9af022212024b4d27
Priti Desai <pdesai@us.ibm.com>
2018-01-26 14:24:51 -0800

    WIP: adding API Gateway functionality

--- deployers/manifestreader.go
+++ deployers/manifestreader.go
@@ -373,12 +373,11 @@ func (reader *ManifestReader) SetRules(rules []*whisk.Rule) error {
 
 func (reader *ManifestReader) SetApis(ar []*whisk.ApiCreateRequest) error {
 	dep := reader.serviceDeployer
-	var apis []*whisk.ApiCreateRequest = make([]*whisk.ApiCreateRequest, 0)
 
 	dep.mt.Lock()
 	defer dep.mt.Unlock()
 
-	for _, api := range apis {
+	for _, api := range ar {
 		existApi, exist := dep.Deployment.Apis[api.ApiDoc.ApiName]
 		if exist {
 			existApi.ApiDoc.ApiName = api.ApiDoc.ApiName
