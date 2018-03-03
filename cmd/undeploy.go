873e1688eb6e305106d992b444c22c6d32652fc2
Priti Desai <pdesai@us.ibm.com>
2018-03-02 17:36:29 -0800

    Deprecating Application in favor of Project (#764)
    * first draft
    
    * replacing application with project
    
    * removing debug print statement
    
    * undo newline
    
    * need to figure out go fmt into Intellij
    
    * adding newline in md

--- cmd/undeploy.go
+++ cmd/undeploy.go
@@ -18,7 +18,6 @@
 package cmd
 
 import (
-	"github.com/apache/incubator-openwhisk-wskdeploy/utils"
 	"github.com/spf13/cobra"
 )
 
@@ -38,20 +37,4 @@ func UndeployCmdImp(cmd *cobra.Command, args []string) error {
 
 func init() {
 	RootCmd.AddCommand(undeployCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// undeployCmd.PersistentFlags().String("foo", "", "A help for foo")
-	undeployCmd.PersistentFlags().StringVar(&utils.Flags.CfgFile, "config", "", "config file (default is $HOME/.wskdeploy.yaml)")
-
-	// TODO() remove the following flags (here) and add to persistent flags in root.go
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// undeployCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")=
-	undeployCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	undeployCmd.Flags().StringVarP(&utils.Flags.ProjectPath, "pathpath", "p", ".", "path to serverless project")
-	undeployCmd.Flags().StringVarP(&utils.Flags.ManifestPath, "manifest", "m", "", "path to manifest file")
-	undeployCmd.Flags().StringVarP(&utils.Flags.DeploymentPath, "deployment", "d", "", "path to deployment file")
 }
--- parsers/yamlparser.go
+++ parsers/yamlparser.go
@@ -25,20 +25,19 @@ import (
 // YAML schema key names
 // DO NOT translate
 const (
-	YAML_KEY_ACTION      = "action"
-	YAML_KEY_ANNOTATION  = "annotoation"
-	YAML_KEY_API         = "api"
-	YAML_KEY_FEED        = "feed"
-	YAML_KEY_MANIFEST    = "manifest"
-	YAML_KEY_NAMESPACE   = "namespace"
-	YAML_KEY_PACKAGES    = "packages"
-	YAML_KEY_PROJECT     = "project"
-	YAML_KEY_RULE        = "rule"
-	YAML_KEY_SEQUENCE    = "sequence"
-	YAML_KEY_TRIGGER     = "trigger"
-	YAML_KEY_APPLICATION = "application" // deprecated
-	YAML_KEY_PACKAGE     = "package"     // deprecated
-	YAML_KEY_SOURCE      = "source"      // deprecated
+	YAML_KEY_ACTION     = "action"
+	YAML_KEY_ANNOTATION = "annotoation"
+	YAML_KEY_API        = "api"
+	YAML_KEY_FEED       = "feed"
+	YAML_KEY_MANIFEST   = "manifest"
+	YAML_KEY_NAMESPACE  = "namespace"
+	YAML_KEY_PACKAGES   = "packages"
+	YAML_KEY_PROJECT    = "project"
+	YAML_KEY_RULE       = "rule"
+	YAML_KEY_SEQUENCE   = "sequence"
+	YAML_KEY_TRIGGER    = "trigger"
+	YAML_KEY_PACKAGE    = "package"
+	YAML_KEY_SOURCE     = "source" // deprecated
 )
 
 // YAML schema key values
@@ -222,9 +221,8 @@ type Project struct {
 }
 
 type YAML struct {
-	Application Project            `yaml:"application"` //used in deployment.yaml (being deprecated)
-	Project     Project            `yaml:"project"`     //used in deployment.yaml
-	Packages    map[string]Package `yaml:"packages"`    //used in deployment.yaml
+	Project  Project            `yaml:"project"`  //used in deployment.yaml
+	Packages map[string]Package `yaml:"packages"` //used in deployment.yaml
 	//Package     Package            `yaml:"package"`   // DEPRECATED.  Should we add warning if found?
 	Filepath string //file path of the yaml file
 }
@@ -232,10 +230,7 @@ type YAML struct {
 // function to return Project or Application depending on what is specified in
 // manifest and deployment files
 func (yaml *YAML) GetProject() Project {
-	if yaml.Application.Name == "" {
-		return yaml.Project
-	}
-	return yaml.Application
+	return yaml.Project
 }
 
 func convertPackageName(packageMap map[string]Package) map[string]Package {
@@ -253,11 +248,7 @@ func convertPackageName(packageMap map[string]Package) map[string]Package {
 }
 
 func ReadEnvVariable(yaml *YAML) *YAML {
-	if yaml.Application.Name != "" {
-		yaml.Application.Packages = convertPackageName(yaml.Application.Packages)
-	} else {
-		yaml.Project.Packages = convertPackageName(yaml.Project.Packages)
-	}
+	yaml.Project.Packages = convertPackageName(yaml.Project.Packages)
 	yaml.Packages = convertPackageName(yaml.Packages)
 	return yaml
 }
