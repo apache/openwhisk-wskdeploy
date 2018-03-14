5a04410d8ea76bfc012ad3740fce17b0451f4911
Pavel Kravchenko <kpavel@il.ibm.com>
2018-03-14 19:20:20 +0200

    Fixes error when empty api map passed to manifest

--- parsers/manifest_parser.go
+++ parsers/manifest_parser.go
@@ -994,7 +994,7 @@ func (dm *YAMLParser) ComposeApiRecordsFromAllPackages(client *whisk.Config, man
 func (dm *YAMLParser) ComposeApiRecords(client *whisk.Config, packageName string, pkg Package, manifestPath string) ([]*whisk.ApiCreateRequest, error) {
 	var requests []*whisk.ApiCreateRequest = make([]*whisk.ApiCreateRequest, 0)
 
-	if pkg.Apis != nil {
+	if pkg.Apis != nil && len(pkg.Apis) != 0 {
 		// verify APIGW_ACCESS_TOKEN is set before composing APIs
 		// until this point, we dont know whether APIs are specified in manifest or not
 		if len(client.ApigwAccessToken) == 0 {
