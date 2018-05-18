074d58fa66bac9f22986142b6ec691026cd94f1f
Priti Desai <pdesai@us.ibm.com>
2018-05-18 14:48:25 -0700

    fixing mini header in action source files

--- tests/src/integration/helloworld/actions/hello.swift
+++ tests/src/integration/helloworld/actions/hello.swift
@@ -1,7 +1,5 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more contributor
- * license agreements; and to You under the Apache License, Version 2.0.
- */
+// Licensed to the Apache Software Foundation (ASF) under one or more contributor
+// license agreements; and to You under the Apache License, Version 2.0.
 
 func main(args: [String:Any]) -> [String:Any] {
     var msg = ["greeting": "Hello stranger!"]
--- tests/src/integration/runtimetests/src/hello.swift
+++ tests/src/integration/runtimetests/src/hello.swift
@@ -1,7 +1,5 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more contributor
- * license agreements; and to You under the Apache License, Version 2.0.
- */
+// Licensed to the Apache Software Foundation (ASF) under one or more contributor
+// license agreements; and to You under the Apache License, Version 2.0.
 
 func main(args: [String:Any]) -> [String:Any] {
     if let name = args["name"] as? String {
