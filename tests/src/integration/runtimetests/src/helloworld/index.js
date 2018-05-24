6bbaf66a1f9f9066a70811d6d253688c73e0d49d
Priti Desai <pdesai@us.ibm.com>
2018-05-24 13:22:40 -0700

    getting rid of node modules

--- tests/src/integration/runtimetests/src/helloworld/index.js
+++ tests/src/integration/runtimetests/src/helloworld/index.js
@@ -1,11 +1,16 @@
 // Licensed to the Apache Software Foundation (ASF) under one or more contributor
 // license agreements; and to You under the Apache License, Version 2.0.
 
-function helloworld(params) {
-    var format = require('string-format');
-    var name = params.name || 'Stranger';
-    payload = format('Hello, {}!', name)
-    return { message: payload };
+/**
+ * Return a simple greeting message for someone.
+ *
+ * @param name A person's name.
+ * @param place Where the person is from.
+ */
+function main(params) {
+    var name = params.name || params.payload || 'stranger';
+    var place = params.place || 'somewhere';
+    return {payload:  'Hello, ' + name + ' from ' + place + '!'};
 }
+exports.main = main;
 
-exports.main = helloworld;
