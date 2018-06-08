a60edc58cc90d66982e3a8e66d3212ccb7616c97
Priti Desai <pdesai@us.ibm.com>
2018-06-07 17:43:59 -0700

    fixing integration test

--- tests/src/integration/apigateway/apigateway_test.go
+++ tests/src/integration/apigateway/apigateway_test.go
@@ -30,7 +30,7 @@ const PATH = "/src/github.com/apache/incubator-openwhisk-wskdeploy/tests/src/int
 
 func TestApiGateway(t *testing.T) {
 	wskdeploy := common.NewWskdeploy()
-	manifestPath := os.Getenv("GOPATH") + "manifest.yml"
+	manifestPath := os.Getenv("GOPATH") + PATH + "manifest.yml"
 	_, err := wskdeploy.DeployManifestPathOnly(manifestPath)
 	assert.Equal(t, nil, err, "Failed to deploy based on the manifest file.")
 	_, err = wskdeploy.UndeployManifestPathOnly(manifestPath)
@@ -39,7 +39,7 @@ func TestApiGateway(t *testing.T) {
 
 func TestApiGatewayWithParams(t *testing.T) {
 	wskdeploy := common.NewWskdeploy()
-	manifestPath := os.Getenv("GOPATH") + "manifest-apis-with-params.yml"
+	manifestPath := os.Getenv("GOPATH") + PATH + "manifest-apis-with-params.yml"
 	_, err := wskdeploy.DeployManifestPathOnly(manifestPath)
 	assert.Equal(t, nil, err, "Failed to deploy based on the manifest file.")
 	_, err = wskdeploy.UndeployManifestPathOnly(manifestPath)
