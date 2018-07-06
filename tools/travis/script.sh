117ff30f51c6dad2010407415009a70aebe72237
Rodric Rabbah <rodric@gmail.com>
2018-07-06 17:13:55 -0400

    Move API Gateway playbook.

--- tools/travis/script.sh
+++ tools/travis/script.sh
@@ -51,11 +51,10 @@ $ANSIBLE_CMD setup.yml
 $ANSIBLE_CMD prereq.yml
 $ANSIBLE_CMD couchdb.yml
 $ANSIBLE_CMD initdb.yml
-
 $ANSIBLE_CMD wipe.yml
-$ANSIBLE_CMD apigateway.yml
 $ANSIBLE_CMD openwhisk.yml -e '{"openwhisk_cli":{"installation_mode":"remote","remote":{"name":"OpenWhisk_CLI","dest_name":"OpenWhisk_CLI","location":"https://github.com/apache/incubator-openwhisk-cli/releases/download/latest"}}}'
-
+$ANSIBLE_CMD apigateway.yml
+$ANSIBLE_CMD routemgmt.yml
 
 export OPENWHISK_HOME="$(dirname "$TRAVIS_BUILD_DIR")/openwhisk"
 
