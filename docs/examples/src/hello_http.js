166db556dfbe2cdbb54dae1dbfba672558299ef0
Bruno Girin <bruno@imby.bio>
2018-08-31 11:51:15 +0100

    Added missing license headers

--- docs/examples/src/hello_http.js
+++ docs/examples/src/hello_http.js
@@ -1,3 +1,11 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more contributor
+// license agreements; and to You under the Apache License, Version 2.0.
+
+/*
+ * This action returns a greeting wrapped into a HTTP response that can be used
+ * by the API Gateway. It also checks for mandatory attributes and returns a
+ * HTTP error response when any attribute is missing.
+ */
 function main(params) {
     if(params.name && params.place) {
         return {
--- docs/examples/src/hello_http_promise.js
+++ docs/examples/src/hello_http_promise.js
@@ -1,4 +1,13 @@
-function main(params) {
+// Licensed to the Apache Software Foundation (ASF) under one or more contributor
+// license agreements; and to You under the Apache License, Version 2.0.
+
+/*
+ * This action returns a greeting wrapped into a HTTP response that can be used
+ * by the API Gateway. It also checks for mandatory attributes and returns a
+ * HTTP error response when any attribute is missing. It uses Promise.resolve
+ * and Promise.reject to handle the response.
+ */
+ function main(params) {
     if(params.name && params.place) {
         return Promise.resolve({
             body: {
