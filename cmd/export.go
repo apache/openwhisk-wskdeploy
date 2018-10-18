378b4a15bb2ccf4ff645fad2b5a7a50fb79b9b5b
Pavel Kravchenko <kpavel@il.ibm.com>
2018-10-18 03:38:17 +0300

    Enable export verbose output

--- cmd/export.go
+++ cmd/export.go
@@ -116,6 +116,10 @@ func ExportAction(actionName string, packageName string, maniyaml *parsers.YAML,
 }
 
 func exportProject(projectName string, targetManifest string) error {
+
+        whisk.SetVerbose(utils.Flags.Verbose)
+        whisk.SetDebug(utils.Flags.Trace)
+
 	maniyaml := &parsers.YAML{}
 	maniyaml.Project.Name = projectName
 
