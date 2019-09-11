01cfe67c4393e93acc44935c5fc14196283ed137
jiangpch <jiangpengcheng@navercorp.com>
2019-09-11 21:15:40 +0800

    Auto supply a dummy API GW token (#1068)
    * Auto supply a dummy API GW token
    
    * Use package constant and print more clear message

--- parsers/manifest_parser.go
+++ parsers/manifest_parser.go
@@ -52,6 +52,8 @@ const (
 	NATIVE_DOCKER_IMAGE   = "openwhisk/dockerskeleton"
 	PARAM_OPENING_BRACKET = "{"
 	PARAM_CLOSING_BRACKET = "}"
+
+	DUMMY_APIGW_ACCESS_TOKEN = "DUMMY TOKEN"
 )
 
 // Read existing manifest file or create new if none exists
@@ -1148,12 +1150,13 @@ func (dm *YAMLParser) ComposeApiRecordsFromAllPackages(client *whisk.Config, man
 func (dm *YAMLParser) ComposeApiRecords(client *whisk.Config, packageName string, pkg Package, manifestPath string) ([]*whisk.ApiCreateRequest, map[string]*whisk.ApiCreateRequestOptions, error) {
 	var requests = make([]*whisk.ApiCreateRequest, 0)
 
-	// verify APIGW_ACCESS_TOKEN is set before composing APIs
-	// until this point, we dont know whether APIs are specified in manifest or not
+	// supply a dummy API GW token as it is optional
 	if pkg.Apis != nil && len(pkg.Apis) != 0 {
 		if len(client.ApigwAccessToken) == 0 {
-			return nil, nil, wskderrors.NewWhiskClientInvalidConfigError(
-				wski18n.ID_MSG_CONFIG_MISSING_APIGW_ACCESS_TOKEN)
+			warningString := wski18n.T(wski18n.ID_MSG_CONFIG_PROVIDE_DEFAULT_APIGW_ACCESS_TOKEN,
+				map[string]interface{}{wski18n.KEY_DUMMY_TOKEN: DUMMY_APIGW_ACCESS_TOKEN})
+			wskprint.PrintOpenWhiskWarning(warningString)
+			client.ApigwAccessToken = DUMMY_APIGW_ACCESS_TOKEN
 		}
 	}
 
